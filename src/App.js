5b9989b376346425c7c317930f64cf3b4c9ee205
shubham <shubhamyedage101@gmail.com>
2026-09-21 15:13:35 +0530

    exploring get , post ,delete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ const express = require('express');
 
 const app = express();
 
+
+app.get('/example' , (req , res)=>{
+  res.send({userName : 'shubham', sirName : 'Yedage'});
+});
+
+app.post('/example' , (req , res)=>{
+  res.send('post request sent successfully');
+});
+
+app.delete('/example' , (req ,res)=>{
+ res.send('message deleted succefully');
+})
+
 app.use('/test',(req, res)=> {
     res.send('response for the test page');
 });
@@ -12,10 +25,13 @@ app.use('/app',( req , res)=> {
     res.send('response for the app page');
 
 });
- 
+
+
 app.use( '/',(req, res)=>{
     res.send('Hello form the server');
 });
+ 
+
 
 app.listen(4000, ()=> {
     console.log('server started on port 4000');
