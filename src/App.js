a7bd0c31c3440ca04f09044110f855afbc393552
shubham <shubhamyedage101@gmail.com>
2026-09-25 12:08:14 +0530

    order of route handler execution

--- src/App.js
+++ src/App.js
@@ -1,38 +1,67 @@
-
-
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+// app.get("/test", (req, res, next) => {
+//   console.log("first get");
+//   //res.send("first response");
+//   next();
+// });
 
-app.get('/example' , (req , res)=>{
-  res.send({userName : 'shubham', sirName : 'Yedage'});
-});
+// app.get("/test", (req, res, next) => {
+//   console.log("second get");
+//   res.send("second response");
+// });
 
-app.post('/example' , (req , res)=>{
-  res.send('post request sent successfully');
-});
+app.use('/test' , (req, res , next)=> {
+    console.log('response for the test page');
+     
+    //res.send('response 1');
+     next();
 
-app.delete('/example' , (req ,res)=>{
- res.send('message deleted succefully');
-})
+},
+(req, res , next)=> {
+      next();
+    res.send('response 2');
 
-app.use('/test',(req, res)=> {
-    res.send('response for the test page');
-});
+},
+(req, res ,next)=> {
+    next();
+    res.send('response 3');
 
-app.use('/app',( req , res)=> {
-    res.send('response for the app page');
+},
+(req, res ,next)=> {
+    res.send('response 4');
 
-});
+}
+);
 
+// app.get('/example' , (req , res)=>{
+//   res.send({userName : 'shubham', sirName : 'Yedage'});
+// });
 
-app.use( '/',(req, res)=>{
-    res.send('Hello form the server');
-});
- 
+// app.post('/example' , (req , res)=>{
+//   res.send('post request sent successfully');
+// });
 
+// app.delete('/example' , (req ,res)=>{
+//  res.send('message deleted succefully');
+// })
 
-app.listen(4000, ()=> {
-    console.log('server started on port 4000');
+// app.use('/test/:userId/:password/:address',(req, res)=> {
+//     console.log(req.params)
+//     res.send('response for the test page');
+// });
+
+// app.use('/app',( req , res)=> {
+//     res.send('response for the app page');
+
+// });
+
+// app.use( '/',(req, res)=>{
+//     res.send('Hello form the server');
+// });
+
+app.listen(4000, () => {
+  console.log("server started on port 4000");
 });
